393c9a3ac291b7c762520a41027eb4d03e48b721
Jefferson <jefry04@gmail.com>
2021-03-17 10:54:03 -0500

    add key to elements

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ class App extends Component {
       <div style={styles.container}>
         <h1 style={styles.title}>Las tecnologias del Front</h1>
         <ul style={styles.list}>
-          {this.state.list.map((tech) => (
-            <li key={tech.id}>{tech}</li>
+          {this.state.list.map((tech, index) => (
+            <li key={index}>{tech}</li>
           ))}
         </ul>
       </div>
